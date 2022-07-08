cc910e4dc8651dac8199a47b9bec1dc49821a6ae
Wissotsky <Wissotsky@protonmail.com>
2022-07-08 10:27:27 +0300

    Typing, docstrings and Cleanup

--- py_smsify/__init__.py
+++ py_smsify/__init__.py
@@ -9,20 +9,6 @@ basic_table = ("@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ 
 extended_table = ("````````````````````^```````````````````{}`````\\````````````[~]`"
        "|````````````````````````````````````€``````````````````````````")
 
-def message_encode(plaintext):
-    literatedtext = anyascii(plaintext)
-    charbytes = bytearray()
-    for character in literatedtext:
-        character_index = basic_table.find(character);
-        if character_index != -1:
-            charbytes.append(character_index)
-            continue
-        character_index = extended_table.find(character)
-        if character_index != -1:
-            charbytes.append(27) #escape to extended table
-            charbytes.append(character_index)
-    return charbytes
-
 @dataclass
 class SmsMessage:
     body: str
@@ -32,10 +18,29 @@ class SmsMessage:
     length: int = field(init=False)
     segments: int = field(init=False)
     def __post_init__(self):
-        self.encoded_text = message_encode(self.body).decode()
-        self.encoded_bytes = bytes(message_encode(self.body))
+        self.encoded_text = SmsMessage.message_encode(self.body).decode()
+        self.encoded_bytes = bytes(SmsMessage.message_encode(self.body))
         self.length = len(self.encoded_bytes)
-        self.segments = ceil(self.length/(153 if self.twilio else 160))
+        self.segments = ceil(self.length/(153 if self.twilio and self.length>160 else 160))
     
     def __repr__(self):
         return self.encoded_text
+    
+    """Encode a unicode string to a bytearray of SMS characters"""
+    def message_encode(plaintext: str) -> bytearray:
+        literatedtext = anyascii(plaintext)
+        charbytes = bytearray()
+        for character in literatedtext:
+            character_index = basic_table.find(character);
+            if character_index != -1:
+                charbytes.append(character_index)
+                continue
+            character_index = extended_table.find(character)
+            if character_index != -1:
+                charbytes.append(27) #escape to extended table
+                charbytes.append(character_index)
+        return charbytes
+
+    """Encode python string into a GSM-7 python encoded string"""
+    def encode(string: str) -> str:
+        return SmsMessage.message_encode(string).decode()
